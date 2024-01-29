04d60d21c54632786c1bbe8f7aad47f142948359
JonathanDev <jonathanmauro.ferrara@gmail.com>
2024-01-29 23:36:30 +0100

    feat: create box info

--- src/components/InfoBox.vue
+++ src/components/InfoBox.vue
@@ -2,19 +2,44 @@
 import { toRefs } from 'vue'
 const props = defineProps({
   title: { type: String, required: true },
-  content: { type: String }
+  content: { type: String },
+  customStyle: { type: String, default: '' }
 })
 
-const { title, content } = toRefs(props)
+const { title, content, customStyle } = toRefs(props)
 </script>
 
 <template>
-  <div class="box">
-    <div class="box__title">{{ title }}</div>
-    <div class="box__content">
-      <span></span>
+  <div :class="customStyle" class="infobox">
+    <div class="infobox__title">{{ title }}</div>
+    <div class="infobox__content">
+      <span>{{ content }}</span>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.infobox {
+  &__title {
+    text-align: center;
+    font-size: 14px;
+    font-weight: 700;
+    color: #959595;
+    margin-bottom: 10px;
+    text-transform: uppercase;
+    @media screen and (min-width: 768px) {
+      text-align: left;
+    }
+  }
+  &__content {
+    text-align: center;
+    font-size: 32px;
+    font-weight: 700;
+    color: black;
+    overflow-wrap: break-word;
+    @media screen and (min-width: 768px) {
+      text-align: left;
+    }
+  }
+}
+</style>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -12,10 +12,10 @@ import SearchBar from '@/components/SearchBar.vue'
   </header>
 
   <main class="box__wrapper">
-    <InfoBox title="Ip address" />
-    <InfoBox title="Location" />
-    <InfoBox title="timezone" />
-    <InfoBox title="isp" />
+    <InfoBox custom-style="box" title="Ip address" content="192.212.174.101" />
+    <InfoBox custom-style="box" title="Location" content="Brooklyn, NY 10001" />
+    <InfoBox custom-style="box" title="timezone" content="UTC - 05:00" />
+    <InfoBox custom-style="box" title="isp" content="SpaceX Starlink" />
   </main>
 </template>
 
@@ -39,6 +39,42 @@ import SearchBar from '@/components/SearchBar.vue'
     line-height: 48px;
     padding-top: 20px;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+}
+
+.box__wrapper {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 80%;
+
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+  margin-inline: auto;
+  margin-top: -100px;
+  z-index: 1;
+  padding: 20px;
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    height: 200px;
+  }
+  .box {
+    width: 100%;
+    padding: 0 20px;
+    margin-top: 20px;
+    @media screen and (min-width: 768px) {
+      width: 25%;
+      margin-top: 0;
+    }
+  }
+  .box:not(:last-child) {
+    border-bottom: 1px solid #ccc;
+    padding-bottom: 20px;
+    @media screen and (min-width: 768px) {
+      border-bottom: none;
+      border-right: 1px solid #ccc;
+    }
   }
 }
 </style>
